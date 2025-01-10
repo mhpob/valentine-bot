d77ae6eb991431b945d3280f014c666dab83a1ad
Mike O'Brien <mikeob9@gmail.com>
2025-01-10 15:19:47 -0500

    add archives

--- valentine-bot.R
+++ valentine-bot.R
@@ -8,13 +8,17 @@ extract_tag <- function(html, tag){
     html_text()
 }
 
+dir <- sample(c("VALCOLL", "VALARCH"), 1)
+table <- ifelse(dir == "VALCOLL", "objects", "group")
+
 n_objects <- request('https://valentine.rediscoverysoftware.com/ProficioWcfServices/ProficioWcfService.svc/FindItXmlStringPageCount') |> 
   req_body_json(
-    list(Words = "*",
-         TableName = "objects",
-         Directory = "VALCOLL",
-         OnlyIfImages = "false",
-         AttachedMediaOption = " "
+    list(
+      Directory = dir,
+      TableName = table,
+      Words = "*",
+      OnlyIfImages = "false",
+      AttachedMediaOption = " "
     )
   ) |> 
   req_perform() |> 
@@ -26,9 +30,9 @@ catalog <- request("https://valentine.rediscoverysoftware.com/ProficioWcfService
   req_body_json(
     list(
       Words= "*",
-      TableName= "objects",
-      Directory= "VALCOLL",
-      FieldList= "cat_nbr",
+      TableName= table,
+      Directory= dir,
+      FieldList= ifelse(dir == "VALCOLL", "cat_nbr", "group_nbr"),
       SortFields = "sortable15",
       OnlyIfImages = "false",
       IncludeImage = "false",
@@ -41,9 +45,66 @@ catalog <- request("https://valentine.rediscoverysoftware.com/ProficioWcfService
   resp_body_json() |> 
   _$d |> 
   read_html() |> 
-  html_elements(xpath = '//objects') |> 
+  html_elements(xpath = ifelse(dir == "VALCOLL", '//objects', '//group')) |> 
   html_text()
 
+
+if(dir == "VALARCH"){
+  catalog <- catalog[grepl("PHC", catalog)]
+  indiv_box <- NULL
+  
+  while(length(indiv_box) == 0){
+    collection <- sample(catalog, 1)
+    box_list <- 'https://valentine.rediscoverysoftware.com/ProficioWcfServices/ProficioWcfService.svc/GetArchivalChildren' |> 
+      request() |> 
+      req_body_json(
+        list(
+          TableName = "GROUP",
+          ArchivalNumber = collection,
+          Directory = "VALARCH"
+        )
+      ) |> 
+      req_perform() |> 
+      resp_body_json() |> 
+      _$d |> 
+      read_html() |> 
+      html_elements(xpath = '//archivallist/archivalnumber') |> 
+      html_text()
+    
+    if(any(grepl("X", box_list))) {
+      item_id <- sample(box_list, 1)
+    } else{
+      i <- 1
+      
+      while(length(indiv_box) == 0 & i <= length(box_list)){
+        indiv_box <- 'https://valentine.rediscoverysoftware.com/ProficioWcfServices/ProficioWcfService.svc/GetArchivalChildren' |> 
+          request() |> 
+          req_body_json(
+            list(
+              TableName = "FILEUNIT",
+              ArchivalNumber = sample(box_list, 1),
+              Directory = "VALARCH"
+            )
+          ) |> 
+          req_perform() |> 
+          resp_body_json() |> 
+          _$d |> 
+          read_html() |> 
+          html_elements(xpath = '//archivallist')
+        
+        i <- i+1
+      }
+    }
+    
+    ids <- indiv_box |>
+      html_elements(xpath ='//archivalnumber') |>
+      html_text()
+    
+    item_id <- sample(ids, 1)
+  }
+} else {
+  item_id <- sample(catalog, 1)
+}
 
 
 
@@ -51,12 +112,16 @@ item <- "https://valentine.rediscoverysoftware.com/ProficioWcfServices/ProficioW
   request() |> 
   req_body_json(
     list(
-      TableName= "objects",
-      Directory="VALCOLL",
-      FieldList="record_id,file_name,ExhibitId,db_exhibt_exhibt_dsc,cat_nbr,cat_nam,form,artist,categ_16,obj_mem,material,type,categ_4",
+      TableName = ifelse(dir == "VALCOLL", "objects", "biblio"),
+      Directory = dir,
+      FieldList =
+        ifelse(dir == "VALCOLL", 
+               "record_id,file_name,ExhibitId,db_exhibt_exhibt_dsc,cat_nbr,cat_nam,form,artist,categ_16,obj_mem,material,type,categ_4",
+               "record_id,biblio_nbr,group_nbr,series_nbr,fileunit_nbr,edition,title,author,sortable14,origin,categ_12,categ_16,categ_1,categ_9[2],categ_3,user_2,user_4,sub_pers,sub_corp,sub_topic,sub_geo,categ_6,categ_7,categ_8,categ_13,subjects,categ_20,file_name"
+        ),
       IncludeImage="true",
       RecordID=-1,
-      readablePrimaryKey=sample(catalog, 1)
+      readablePrimaryKey=item_id
     )
   ) |> 
   req_perform() |> 
@@ -64,22 +129,33 @@ item <- "https://valentine.rediscoverysoftware.com/ProficioWcfServices/ProficioW
   _$d |> 
   read_html()
 
-info <- data.frame(
-  catalog_number = extract_tag(item, 'cat_nbr'),
-  file_name = extract_tag(item, "file_name"),
-  category = extract_tag(item, "cat_nam"),
-  date = extract_tag(item, "categ_16"),
-  artist = extract_tag(item, "artist"),
-  description = extract_tag(item, "obj_mem"),
-  collection = extract_tag(item, "categ_4")
-)
+if(dir == "VALCOLL"){
+  item_info <- data.frame(
+    id = extract_tag(item, 'cat_nbr'),
+    file_name = extract_tag(item, "file_name"),
+    category = extract_tag(item, "cat_nam"),
+    date = extract_tag(item, "categ_16"),
+    creator = extract_tag(item, "artist"),
+    description = extract_tag(item, "obj_mem"),
+    collection = extract_tag(item, "categ_4")
+  )
+} else {
+  item_info <- data.frame(
+    title = extract_tag(item, "title"),
+    id = item_id,
+    date = extract_tag(item, 'origin'),
+    description = extract_tag(item, "categ_16"),
+    file_name = extract_tag(item, "file_name"),
+    creator = extract_tag(item, "author")
+  )
+}
 
 images <- 'https://valentine.rediscoverysoftware.com/ProficioWcfServices/ProficioWcfService.svc/GetImagePaths'|> 
   request() |> 
   req_body_json(
     list(
-      Directory = "VACOLL",
-      ImageKey = info$file_name,
+      Directory = dir,
+      ImageKey = item_info$file_name,
       FieldList="imagedescription"
     )
   ) |> 
@@ -94,6 +170,25 @@ image_info <- data.frame(
 
 
 
+
+paste0("https://valentine.rediscoverysoftware.com/",
+       ifelse(dir == "VALCOLL", "mDetail", "MADetailB"),
+       ".aspx?rID=",
+       item_info$id,
+       "&db=",
+       ifelse(dir == "VALCOLL", 'objects', 'biblio'),
+       "&dir=",
+       dir) |> 
+  browseURL()
+
+paste0(
+  "https://valentine.rediscoverysoftware.com/FullImages",
+  image_info$image_path[sample(1:length(image_info$image_path), 1)]) |> 
+  browseURL()
+
+
+### update below
+
 auth(
   user = 'thevalentinebot.bsky.social',
   password = Sys.getenv("BSKY_PAT")
@@ -101,20 +196,23 @@ auth(
 post(
   text = paste0(
     "Cat #: ",
-    info$catalog_number,
+    item_info$id,
     "\nDate: ",
-    info$date,
+    item_info$date,
     "\nArtist: ",
-    info$artist,
-    "\nArea: ",
-    info$collection,
+    item_info$creator,
     "\n",
-    paste0("https://valentine.rediscoverysoftware.com/mDetail.aspx?rID=",
-    info$catalog_number,
-    "&db=objects&dir=VALCOLL")
+    paste0("https://valentine.rediscoverysoftware.com/",
+           ifelse(dir == "VALCOLL", "mDetail", "MADetailB"),
+           ".aspx?rID=",
+           item_info$id,
+           "&db=",
+           ifelse(dir == "VALCOLL", 'objects', 'biblio'),
+           "&dir=",
+           dir)
   ),
   image = paste0(
     "https://valentine.rediscoverysoftware.com/FullImages",
     image_info$image_path[sample(1:length(image_info$image_path))]),
-  image_alt = info$description
+  image_alt = item_info$description
 )
