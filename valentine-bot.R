76f4f723f399d2f4353d492c8db44c3ad3ff7324
Mike O'Brien <mikeob9@gmail.com>
2025-03-06 15:37:18 -0500

    Add User-Agent. Closes #3

--- valentine-bot.R
+++ valentine-bot.R
@@ -2,47 +2,49 @@ library(httr2)
 library(rvest)
 library(atrrr)
 
-extract_tag <- function(html, tag){
-  html |> 
-    html_elements(tag) |> 
-    html_text() |> 
+extract_tag <- function(html, tag) {
+  html |>
+    html_elements(tag) |>
+    html_text() |>
     trimws()
 }
 
 poll_catalog <- function(dir) {
-  
-  if(dir == "VALCOLL") {
+  if (dir == "VALCOLL") {
     table <- "objects"
     search_term <- "*"
     fields <- "cat_nbr"
-    
+
     cat("Searching collections...", sep = "")
-  } else{
+  } else {
     table <- "biblio"
     # Making up a random search term based on numbers
     search_term <- sample(c(0:format(Sys.Date(), "%Y"), "Cook"), 1)
     fields <- "biblio_nbr,group_nbr,series_nbr,fileunit_nbr"
-    
+
     cat("Searching archives for \"", search_term, "\"...", sep = "")
   }
-  
-  request("https://valentine.rediscoverysoftware.com/ProficioWcfServices/ProficioWcfService.svc/FindItXmlStringPage") |> 
+
+  request(
+    "https://valentine.rediscoverysoftware.com/ProficioWcfServices/ProficioWcfService.svc/FindItXmlStringPage"
+  ) |>
     req_body_json(
       list(
-        Words= search_term,
-        TableName= table,
-        Directory= dir,
-        FieldList= fields,
+        Words = search_term,
+        TableName = table,
+        Directory = dir,
+        FieldList = fields,
         SortFields = "sortable15",
         OnlyIfImages = "false",
         IncludeImage = "false",
         RecordsPerPage = -1,
         AttachedMediaOption = " "
       )
-    ) |> 
-    req_perform() |> 
-    resp_body_json() |> 
-    _$d |> 
+    ) |>
+    req_user_agent("Valentine Bot (https://github.com/mhpob/valentine-bot)") |>
+    req_perform() |>
+    resp_body_json() |>
+    _$d |>
     read_html()
 }
 
@@ -50,14 +52,17 @@ poll_catalog <- function(dir) {
 dir <- sample(c("VALCOLL", "VALARCH"), 1)
 catalog <- poll_catalog(dir)
 
-if(dir == "VALCOLL") {
-  catalog <- catalog |> 
-    extract_tag('objects') 
+if (dir == "VALCOLL") {
+  catalog <- catalog |>
+    extract_tag('objects')
 } else {
   catalog <- paste0(
-    extract_tag(catalog, 'group_nbr'), "/", 
-    extract_tag(catalog, 'series_nbr'), "-", 
-    extract_tag(catalog, 'fileunit_nbr'), "#",
+    extract_tag(catalog, 'group_nbr'),
+    "/",
+    extract_tag(catalog, 'series_nbr'),
+    "-",
+    extract_tag(catalog, 'fileunit_nbr'),
+    "#",
     extract_tag(catalog, 'biblio_nbr')
   )
   # Archives contain manuscript collections ("MSC") and photo collections ("PHC")
@@ -67,47 +72,47 @@ if(dir == "VALCOLL") {
 
 cat("done.\n", length(catalog), "items found.\nSelecting item...\n")
 
-while(TRUE){
+while (TRUE) {
   item <- sample(catalog, 1)
-  item_record <- "https://valentine.rediscoverysoftware.com/ProficioWcfServices/ProficioWcfService.svc/GetRecordDetails" |> 
-    request() |> 
+  item_record <- "https://valentine.rediscoverysoftware.com/ProficioWcfServices/ProficioWcfService.svc/GetRecordDetails" |>
+    request() |>
     req_body_json(
       list(
         TableName = ifelse(dir == "VALCOLL", "objects", "biblio"),
         Directory = dir,
-        FieldList =
-          ifelse(dir == "VALCOLL", 
-                 "record_id,file_name,ExhibitId,db_exhibt_exhibt_dsc,cat_nbr,cat_nam,form,artist,categ_16,obj_mem,material,type,categ_4",
-                 "record_id,biblio_nbr,group_nbr,series_nbr,fileunit_nbr,edition,title,author,sortable14,origin,categ_12,categ_16,categ_1,categ_9[2],categ_3,user_2,user_4,sub_pers,sub_corp,sub_topic,sub_geo,categ_6,categ_7,categ_8,categ_13,subjects,categ_20,file_name"
-          ),
-        IncludeImage="true",
-        RecordID=-1,
-        readablePrimaryKey=item
+        FieldList = ifelse(
+          dir == "VALCOLL",
+          "record_id,file_name,ExhibitId,db_exhibt_exhibt_dsc,cat_nbr,cat_nam,form,artist,categ_16,obj_mem,material,type,categ_4",
+          "record_id,biblio_nbr,group_nbr,series_nbr,fileunit_nbr,edition,title,author,sortable14,origin,categ_12,categ_16,categ_1,categ_9[2],categ_3,user_2,user_4,sub_pers,sub_corp,sub_topic,sub_geo,categ_6,categ_7,categ_8,categ_13,subjects,categ_20,file_name"
+        ),
+        IncludeImage = "true",
+        RecordID = -1,
+        readablePrimaryKey = item
       )
-    ) |> 
-    req_perform() |> 
-    resp_body_json() |> 
-    _$d |> 
+    ) |>
+    req_perform() |>
+    resp_body_json() |>
+    _$d |>
     read_html()
-  
+
   item_info <- data.frame(
-    id = item, 
+    id = item,
     file_name = extract_tag(item_record, 'file_name')
   )
-  
-  image_check <- 'https://valentine.rediscoverysoftware.com/ProficioWcfServices/ProficioWcfService.svc/GetImagePaths'|> 
-    request() |> 
+
+  image_check <- 'https://valentine.rediscoverysoftware.com/ProficioWcfServices/ProficioWcfService.svc/GetImagePaths' |>
+    request() |>
     req_body_json(
       list(
         Directory = dir,
         ImageKey = item_info$file_name,
-        FieldList="imagedescription"
+        FieldList = "imagedescription"
       )
-    ) |> 
-    req_perform() |> 
-    resp_body_json() |> 
-    _$d 
-  
+    ) |>
+    req_perform() |>
+    resp_body_json() |>
+    _$d
+
   if (length(image_check >= 1)) break
 }
 
@@ -116,7 +121,7 @@ while(TRUE){
 cat(item_info$id, '\nGetting item record...', sep = '')
 
 
-if(dir == "VALCOLL"){
+if (dir == "VALCOLL") {
   item_info <- data.frame(
     id = extract_tag(item_record, 'cat_nbr'),
     title = extract_tag(item_record, "cat_nam"),
@@ -137,7 +142,7 @@ if(dir == "VALCOLL"){
   )
 }
 
-for (i in 1:ncol(item_info)){
+for (i in 1:ncol(item_info)) {
   item_info[, i] <- gsub("\\s?(--|__)", "; ", x = item_info[, i])
 }
 
@@ -151,27 +156,27 @@ item_url <- paste0(
   "&dir=",
   dir
 ) |>
-  URLencode() |> 
+  URLencode() |>
   gsub(",", "%2C", x = _)
 
 cat("done.\nItem URL:", item_url, "\nFinding image URLs...")
 
-images <- 'https://valentine.rediscoverysoftware.com/ProficioWcfServices/ProficioWcfService.svc/GetImagePaths'|> 
-  request() |> 
+images <- 'https://valentine.rediscoverysoftware.com/ProficioWcfServices/ProficioWcfService.svc/GetImagePaths' |>
+  request() |>
   req_body_json(
     list(
       Directory = dir,
       ImageKey = item_info$file_name,
-      FieldList="imagedescription"
+      FieldList = "imagedescription"
     )
-  ) |> 
-  req_perform() |> 
-  resp_body_json() |> 
-  _$d |> 
+  ) |>
+  req_perform() |>
+  resp_body_json() |>
+  _$d |>
   read_html()
 
 image_info <- data.frame(
-  image_path = extract_tag(images, "fullimage") |> 
+  image_path = extract_tag(images, "fullimage") |>
     gsub("\\\\", "/", x = _)
 )
 image_info$url <- paste0(
@@ -184,9 +189,9 @@ cat('done.\nImage URLs:\n', paste(image_info$url, collapse = "\n"))
 
 
 # Can only post 4 media items at a time
-if(nrow(image_info) > 4){
+if (nrow(image_info) > 4) {
   image_info <- image_info[sample(1:nrow(image_info), 4), ]
-  image_info <- image_info[order(row.names(image_info)),]
+  image_info <- image_info[order(row.names(image_info)), ]
 }
 
 ## Uncomment for interactive checks
@@ -199,14 +204,12 @@ if(nrow(image_info) > 4){
 #        "&dir=",
 #        dir) |>
 #   browseURL()
-# 
+#
 # paste0(
 #   "https://valentine.rediscoverysoftware.com/FullImages",
-#   image_info$image_path[sample(1:length(image_info$image_path), 1)]) |> 
-#   URLencode() |> 
+#   image_info$image_path[sample(1:length(image_info$image_path), 1)]) |>
+#   URLencode() |>
 #   browseURL()
-
-
 
 ### Skeet it
 
@@ -220,15 +223,14 @@ auth(
 
 post_skeet(
   text = paste0(
-    "ID: ", item_info$id,
-    ifelse(item_info$title != "", paste0("\n",item_info$title), ""),
-    ifelse(item_info$date != "",
-           paste0("\nDate: ", item_info$date),
-           ""
-    ),
-    ifelse(item_info$creator != "",
-           paste0("\nArtist: ", item_info$creator),
-           ""
+    "ID: ",
+    item_info$id,
+    ifelse(item_info$title != "", paste0("\n", item_info$title), ""),
+    ifelse(item_info$date != "", paste0("\nDate: ", item_info$date), ""),
+    ifelse(
+      item_info$creator != "",
+      paste0("\nArtist: ", item_info$creator),
+      ""
     ),
     "\n",
     item_url
@@ -238,4 +240,3 @@ post_skeet(
 )
 
 cat("done.")
-
